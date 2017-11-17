9efb368e14a362d91825ba8a9b83adb5ea771928
C-Rodg <rodgersbc@gmail.com>
2017-11-17 09:16:30 -0800

    Started webpack builds

--- webpack.build.config.js
+++ webpack.build.config.js
@@ -6,3 +6,36 @@ const webpack = require("webpack"),
 
 const SRC_DIR = path.resolve(__dirname, "src");
 const OUTPUT_DIR = path.resolve(__dirname, "dist");
+
+const defaultInclude = [SRC_DIR];
+
+module.exports = {
+	entry: SRC_DIR + "/index.js",
+	output: {
+		path: OUTPUT_DIR,
+		publicPath: "./",
+		filename: "bundle.js"
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: ExtractTextPlugin.extract({
+					fallback: "style-loader",
+					use: "css-loader"
+				}),
+				include: defaultInclude
+			},
+			{
+				test: /\.jsx?$/,
+				use: [{ loader: "babel-loader" }],
+				include: defaultInclude
+			},
+			{
+				test: /\.(jpg?g|png|gif)$/,
+				use: [{ loader: "file-loader?name=img/[name]__[hash:base64:5].[ext]" }],
+				include: defaultInclude
+			}
+		]
+	}
+};

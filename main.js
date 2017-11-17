e3affa7d6a514d1e69c36f11e36674c9353c931b
C-Rodg <rodgersbc@gmail.com>
2017-11-17 09:56:50 -0800

    Setup project directory structure

---
+++ main.js
@@ -0,0 +1,11 @@
+const menubar = require("menubar");
+
+const mb = menubar({
+	tooltip: "One click away!",
+	icon: "src/static/icon.png",
+	index: Path.join("file://", __dirname, "index.html")
+});
+
+mb.on("ready", () => {
+	console.log("App is ready!");
+});

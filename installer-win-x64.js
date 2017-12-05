a1a0f64380f6fd5625dfcec2f4c4206495bf8dc8
C-Rodg <rodgersbc@gmail.com>
2017-12-05 15:51:12 -0800

    Changed 32-bit naming

--- installer-win-x64.js
+++ installer-win-x64.js
@@ -2,14 +2,14 @@ const electronInstaller = require("electron-winstaller"),
 	path = require("path");
 
 const resultPromise = electronInstaller.createWindowsInstaller({
-	appDirectory: "./builds/output/Coinbar-win32-ia32",
-	outputDirectory: "./builds/releases/Coinbar_win_32",
+	appDirectory: "./builds/output/Coinbar-win32-x64",
+	outputDirectory: "./builds/releases/Coinbar_win_x64",
 	exe: "Coinbar.exe",
 	version: "1.0.1",
-	setupExe: "CoinbarInstaller_32.exe"
+	setupExe: "CoinbarInstaller_x64.exe"
 });
 
 resultPromise.then(
-	() => console.log("Build for Win32 complete!"),
+	() => console.log("Build for Win64 complete!"),
 	e => console.log(e)
 );
--- installer-win-x86.js
+++ installer-win-x86.js
@@ -2,14 +2,14 @@ const electronInstaller = require("electron-winstaller"),
 	path = require("path");
 
 const resultPromise = electronInstaller.createWindowsInstaller({
-	appDirectory: "./builds/output/Coinbar-win32-x64",
-	outputDirectory: "./builds/releases/Coinbar_win_64",
+	appDirectory: "./builds/output/Coinbar-win32-ia32",
+	outputDirectory: "./builds/releases/Coinbar_win_x86",
 	exe: "Coinbar.exe",
 	version: "1.0.1",
-	setupExe: "CoinbarInstaller_64.exe"
+	setupExe: "CoinbarInstaller_x86.exe"
 });
 
 resultPromise.then(
-	() => console.log("Build for Win64 complete!"),
+	() => console.log("Build for Win32 complete!"),
 	e => console.log(e)
 );
